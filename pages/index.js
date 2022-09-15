32c9a95710600517b5eea1c792b571936385cbf2
SaiPraveen07 <107098201+SaiPraveen07@users.noreply.github.com>
2022-09-15 14:11:19 +0530

    pages and routing

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Development Project</title>
+        <meta name="keywords" content="web development ,programming"></meta>
+      </Head>
+
+      <h1>Welcome to Nextjs</h1>
+    </div>
+  );
+}
